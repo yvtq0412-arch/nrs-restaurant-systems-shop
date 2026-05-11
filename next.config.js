6f6be3366f29123355a9fbc4a3ce7795f5b9539f
system <yvtq0412@gmail.com>
2026-05-11 16:25:06 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,12 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // firebase-admin など Node.js 専用パッケージはサーバーバンドルに含めず外部解決させる
+  // これで「Critical dependency: the request of a dependency is an expression」の警告が消える
+  serverExternalPackages: ['firebase-admin'],
+
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
       { protocol: 'https', hostname: 'storage.googleapis.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
+  },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // firebase-admin の依存解析エラーを抑制
+      config.ignoreWarnings = [
+        ...(config.ignoreWarnings ?? []),
+        { module: /node_modules\/@grpc\/grpc-js/ },
+        { module: /node_modules\/@opentelemetry/ },
+        { module: /node_modules\/firebase-admin/ },
+        { message: /Critical dependency: the request of a dependency is an expression/ },
+      ];
+    }
+    return config;
   },
 };
 
